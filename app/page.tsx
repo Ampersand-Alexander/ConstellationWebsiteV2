8f31a618a1e66bb321914c30e36928440c164cda
Alex Tran <Fujinxyukinko@gmail.com>
2023-08-28 01:31:05 -0700

    added Ruby and greetings page

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,21 @@
 import Image from 'next/image'
 import Spline from '@splinetool/react-spline';
 
+import PreLoader from "./components/other/PreLoader";
+
+
 import Greetings from "./sections/greetings"
+import Ruby from "./sections/ruby"
 
 export default function Home() {
   return (
-    <Greetings />
+    <>
+      <PreLoader />
+      <main
+                className="flex flex-col items-center justify-center bg-black"
+            ></main>
+        <Greetings />
+        <Ruby />
+    </>
   )
 }

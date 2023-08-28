f15e487fd1f273a1d225bc43872197cfae79336f
Alex Tran <60246286+Ampersand-Alexander@users.noreply.github.com>
2023-08-27 22:12:01 -0700

    setting up constellation

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import Image from 'next/image'
+import Spline from '@splinetool/react-spline';
+
+import Greetings from "./sections/greetings"
+
+export default function Home() {
+  return (
+    <Greetings />
+  )
+}

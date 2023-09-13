8b0dc783f4e63debd932af61c3884baa4e9adc51
Alex Tran <Fujinxyukinko@gmail.com>
2023-09-13 16:36:39 -0700

    added background to introduction

--- app/sections/introduction.tsx
+++ app/sections/introduction.tsx
@@ -8,8 +8,14 @@ import AnimatedTitle from "../animations/AnimatedTitle";
 const Introduction = () => {
     return (
         <section
-            className="relative z-300 w-full items-center justify-center overflow-hidden bg-[#000000] bg-cover bg-center pt-16 pb-36 md:pt-20 md:pb-44 lg:pt-30 lg:pb-56"
+            className="relative z-300 w-full items-center justify-center overflow-hidden pt-16 pb-36 md:pt-20 md:pb-44 lg:pt-30 lg:pb-56"
             id="about"
+            style={{
+                backgroundImage: `url(${"/introfadebckground.png"})`, // Set the background image here
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundColor: '#000000', // Fallback background color
+            }}
         >
             <div className="mx-auto flex w-[100%] flex-col items-start lg:max-w-[1212.8px]">
                 <div className="mb-16 lg:mb-32"></div> {/* Add whitespace above */}

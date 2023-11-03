81bdf4568a7457767363bd7bb931cf279248374a
Alex Tran <Fujinxyukinko@gmail.com>
2023-11-02 21:10:28 -0700

    project page changes

--- app/sections/projects.tsx
+++ app/sections/projects.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import AnimatedBody from "../animations/AnimatedBody";
 
 const Projects = () => {
   const projectCards = [
@@ -24,22 +25,22 @@ const Projects = () => {
       button: "Powerful"
     },
   ];
-
   return (
-    <section 
-      className="relative w-full items-center justify-center overflow-hidden pt-16 pb-36 md:pt-20 md:pb-44 lg:pt-30 lg:pb-56"
-      id="projects"
-     >
-      <div className="mx-auto flex w-[100%] flex-col lg:gap-20 relative z-10 w-full flex items-center justify-center overflow-hidden bg-[#000000] bg-center pt-16 pb-36 md:pt-20 md:pb-44 lg:pt-20 lg:pb-56" id="greetings h-[100%]">
+    <section className="relative w-full items-center justify-center overflow-hidden pt-16 pb-36 md:pt-20 md:pb-44 lg:pt-30 lg:pb-56" id="projects">
+      <div className="absolute top-0 left-0 mx-auto flex w-[100%] flex-col lg:gap-10 relative z-10 w-full flex items-center justify-left overflow-hidden bg-[#000000] bg-center pt-16 pb-20 md:pt-20 md:pb-44 lg:pt-20 lg:pb-56" id="greetings h-[100%]">
         {projectCards.map((project, index) => (
-          <div className="project-card text-[#000000] rounded-lg p-6 my-4 w-full lg:w-200 " key={index} style={{ backgroundImage: `url(${project.background})`, backgroundSize: 'cover' }}>
-            <div className="overlay" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}></div>
-            <h2>{project.title}</h2>
-            <p>{project.description}</p>
-            <Link href={project.link}>
-              <h1 className="view-button bg-[#000000]">{project.button}</h1>
-            </Link>
-          </div>
+          <Link href={project.link} key={index}>
+            <div className="flex w-[100%] flex-col text-[18px] font-medium text-[#FFFFFF] sm:items-center sm:justify-center md:items-center md:text-[20px] lg:items-center lg:mb-16  lg:max-w-[90%] lg:text-[24px]">
+              <AnimatedBody delay={0.5} text={project.title}/>
+            </div>
+            
+            <div className="project-card text-white rounded-lg p-6 w-full lg:w-200 h-200 border-2 border-customPink relative" style={{ backgroundImage: `url(${project.background})`, backgroundSize: 'cover' }}>
+              <h2 className="overlay z-5">click to see more</h2>
+              <div className="overlay absolute top-0 left-0 w-full h-full bg-black opacity-50 rounded-lg"></div>               
+                  <div className="hover-effect absolute top-0 left-0 w-full h-full bg-customPink opacity-0 rounded-lg transition-opacity duration-300"></div>
+
+            </div>
+          </Link>
         ))}
       </div>  
     </section>
